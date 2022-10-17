403cbf330a92b190507e42b2cbb113ff69b6c1e4
纯纯废物haaaxi <67006593+dohieaibr44@users.noreply.github.com>
2022-10-17 19:48:30 +0800

    Add files via upload

---
+++ 四则运算.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+//#include <iomanip>
+using namespace std;
+
+int main(){
+  int x,y,n,i=1;
+  char k;
+
+  while(233)
+ {
+     cin>>x>>y;
+
+     if(x==0&&y==0)break;
+
+    switch (k)
+    {
+
+        case '+':n=x+y;cout<<n;break;
+
+        case '-':n=x-y;cout<<n;break;
+
+        case '*':n=x*y;cout<<n;break;
+
+        case '/':n=x/y;cout<<n;break'
+
+        default;
+
+    }
+
+ }
+}
